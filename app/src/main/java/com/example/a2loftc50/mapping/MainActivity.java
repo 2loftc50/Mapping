7131740f15431b60ceba28d918ddf0a533ec4f1f
Chloe Lofting <2loftc50@solent.ac.uk>
2018-01-29 12:33:39 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2loftc50/mapping/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a2loftc50.mapping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        MapView mv = (MapView)findViewById(R.id.map1);
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(16);
+        mv.getController().setCenter(new GeoPoint(50.83,-1.05));
+    }
+}
